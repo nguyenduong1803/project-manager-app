19fa689cd401549ce5d0f525b30ec03d2acb4c5f
duonnh200 <duongnh@sphinxjsc.com>
2023-02-10 09:58:00 +0700

    init

---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { Routes } from "@angular/router";
+
+import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
+
+export const AppRoutes: Routes = [
+  {
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full",
+  },
+  {
+    path: "",
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: "",
+        loadChildren: () =>
+          import("./layouts/admin-layout/admin-layout.module").then(
+            (x) => x.AdminLayoutModule
+          ),
+      },
+    ],
+  },
+  {
+    path: "**",
+    redirectTo: "dashboard",
+  },
+];
